8f9f80fc9a51b5a7d06ac1fd083ec7c1d5e6a3f7
Federico18011980 <101432540+Federico18011980@users.noreply.github.com>
2023-03-08 17:24:22 -0300

    task 3 terminada

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,19 @@ const boton = document.querySelector("[data-form-btn]");
 const crearTask = (evento) =>{
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const valor= input.value;
+    const task = document.createElement("li");
+    task.classList.add("card");
+    const ul = document.querySelector("[data-list]");
+    const contenido = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${valor}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML=contenido;
+  ul.appendChild(task);
+    input.value="";
+    
 } 
     
 boton.addEventListener("click",crearTask );
